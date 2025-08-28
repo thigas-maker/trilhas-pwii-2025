a1b634730257fe28e5da92ddc287046890d78a32
thigas-maker <thiago4.souza@alunos.ifsuldeminas.edu.br>
2025-08-28 16:06:52 -0300

    Add files via upload

---
+++ 2.estruturacondicional/switchcase.js
@@ -0,0 +1,11 @@
+let idade = 7;
+switch(idade){
+    case 5:
+        console.log("Criança de 5 anos");
+        break;
+    case 6:
+        console.log("Criança de 6 anos");
+        break;
+    default:
+        console.log("Idade não reconhecida");
+}
